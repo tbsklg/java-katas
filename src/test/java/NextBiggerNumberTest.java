26af158d6c95b0caaf9392439f5ffe1da93a726b
Tobias Klug <tobias.klug@datev.de>
2021-06-09 14:14:41 +0200

    Support two diget numbers

--- src/test/java/NextBiggerNumberTest.java
+++ src/test/java/NextBiggerNumberTest.java
@@ -1,5 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+import static java.lang.Integer.*;
+import static java.lang.Long.parseLong;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class NextBiggerNumberTest {
   public static long nextBiggerNumber(long n) {
-    return n;
+    final var nAsString = String.valueOf(n);
+
+    if (nAsString.length() == 2) {
+      final var first = parseInt(nAsString.substring(0, 1));
+      final var second = parseInt(nAsString.substring(1));
+
+      if (first < second) {
+        return swap(first, second);
+      }
+    }
+
+    return -1;
+  }
+
+  private static long swap(long first, long second) {
+    final var firstAsString = String.valueOf(first);
+    final var secondAsString = String.valueOf(second);
+
+    return parseLong(secondAsString.concat(firstAsString));
+  }
+
+  @Test
+  void shouldCalculateForSingleDigitNumber() {
+    assertThat(nextBiggerNumber(1)).isEqualTo(-1);
+  }
+
+  @Test
+  void shouldCalculateForTwoDigitNumber() {
+    assertThat(nextBiggerNumber(12)).isEqualTo(21);
+    assertThat(nextBiggerNumber(21)).isEqualTo(-1);
   }
 }
