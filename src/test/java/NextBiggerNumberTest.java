34ba992359937090e56c5e7383ae111d38c99e4b
Tobias Klug <tobias.klug@datev.de>
2021-06-10 21:53:01 +0200

    Solve using reversed sorting

--- src/test/java/NextBiggerNumberTest.java
+++ src/test/java/NextBiggerNumberTest.java
@@ -1,23 +1,29 @@
+import org.assertj.core.api.Assertions;
+import org.assertj.core.internal.bytebuddy.build.HashCodeAndEqualsPlugin;
 import org.junit.jupiter.api.Test;
 
-import static java.lang.Integer.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import static java.lang.Integer.parseInt;
 import static java.lang.Long.parseLong;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NextBiggerNumberTest {
   public static long nextBiggerNumber(long n) {
     final var nAsString = String.valueOf(n);
+    final var sorted = List.of(nAsString.split("")).stream()
+            .sorted(Comparator.reverseOrder())
+            .collect(Collectors.joining());
 
-    if (nAsString.length() == 2) {
-      final var first = parseInt(nAsString.substring(0, 1));
-      final var second = parseInt(nAsString.substring(1));
-
-      if (first < second) {
-        return swap(first, second);
-      }
+    if (Long.valueOf(n).equals(Long.valueOf(sorted))) {
+      return -1;
     }
 
-    return -1;
+    return Long.parseLong(sorted);
   }
 
   private static long swap(long first, long second) {
@@ -36,5 +42,14 @@ public class NextBiggerNumberTest {
   void shouldCalculateForTwoDigitNumber() {
     assertThat(nextBiggerNumber(12)).isEqualTo(21);
     assertThat(nextBiggerNumber(21)).isEqualTo(-1);
+    assertThat(nextBiggerNumber(33)).isEqualTo(-1);
+    assertThat(nextBiggerNumber(14)).isEqualTo(41);
+  }
+
+  @Test
+  void shouldCalculateForThreeDigetNumber() {
+    assertThat(nextBiggerNumber(101)).isEqualTo(110);
+    assertThat(nextBiggerNumber(110)).isEqualTo(-1);
+    assertThat(nextBiggerNumber(120)).isEqualTo(210);
   }
 }
