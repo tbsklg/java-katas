fedd4e7e0183531364b950aa361f0ef8e5dd7d79
tbsklg <tbs.klg@gmail.com>
2022-02-22 10:38:08 +0100

    Solve 5536a85b6ed4ee5a78000035: Help your granny

---
+++ src/main/java/kyufive/Tour.java
@@ -0,0 +1,44 @@
+package kyufive;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.util.Arrays.stream;
+import static java.util.stream.Stream.iterate;
+
+public class Tour {
+
+  public static int tour(String[] arrFriends, String[][] ftwns, Map<String, Double> h) {
+    final var distancesForFriends =
+            stream(arrFriends)
+                    .map(f -> stream(ftwns)
+                            .filter(t -> t[0].equals(f))
+                            .findFirst()
+                    )
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .map(t -> t[1])
+                    .map(t -> h.getOrDefault(t, 0.0))
+                    .collect(Collectors.toList());
+
+    final var distancesBetweenFriends =
+            iterate(0, i -> i + 1)
+                    .limit(distancesForFriends.size() - 1)
+                    .map(i -> distance(distancesForFriends.get(i), distancesForFriends.get(i + 1)))
+                    .reduce((double) 0, Double::sum);
+
+    return (int) floor(
+            distancesForFriends.get(0)
+                    + distancesBetweenFriends
+                    + distancesForFriends.get(distancesForFriends.size() - 1)
+    );
+  }
+
+  private static Double distance(Double x, Double y) {
+    return sqrt(pow(y, 2) - pow(x, 2));
+  }
+}
---
+++ src/test/java/kyufive/TourTest.java
@@ -0,0 +1,25 @@
+package kyufive;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TourTest {
+
+  @Test
+  public void test1() {
+    String[] friends1 = new String[]{"A1", "A2", "A3", "A4", "A5"};
+    String[][] fTowns1 = {new String[]{"A1", "X1"}, new String[]{"A2", "X2"}, new String[]{"A3", "X3"},
+            new String[]{"A4", "X4"}};
+    Map<String, Double> distTable1 = new HashMap<String, Double>();
+    distTable1.put("X1", 100.0);
+    distTable1.put("X2", 200.0);
+    distTable1.put("X3", 250.0);
+    distTable1.put("X4", 300.0);
+
+    assertThat(Tour.tour(friends1, fTowns1, distTable1)).isEqualTo(889);
+  }
+}
